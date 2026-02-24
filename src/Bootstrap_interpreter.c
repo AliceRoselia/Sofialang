7a66530b2e2c0f2d0bcec225162853b016e92744
AliceRoselia <63040919+AliceRoselia@users.noreply.github.com>
2026-02-24 16:44:09 +0700

    Starting bootstrap interpreter

---
+++ src/Bootstrap_interpreter.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+#include <conio.h>
+
+
+
+struct primitiveContainer;
+
+struct dictEntry
+{
+// Not sure if this is necessary yet. It might be possible we can force the primitive dicts to be perfect-hashable or something. We don't know yet.
+size_t key;
+size_t value;
+};
+
+
+
+// 8 bytes in normal 64-bits compilers.
+// size_t would be used as raw bytes for numbers.
+union data_ptr
+{
+  size_t* scalar;
+  struct dictEntry* values;
+  struct primitiveContainer* container;
+  struct primitiveContainer (*func) (struct primitiveContainer);
+};
+
+
+struct primitiveContainer{
+  // Primitive string, array, or dict.
+  // Assume a dict unless shown otherwise.
+  //
+  size_t size;
+  union data_ptr data;
+};
+
+
+
+int main(){
+    printf("Compilation passed");
+    //This is the template for everything.
+    getch();
+    return 0;
+}
